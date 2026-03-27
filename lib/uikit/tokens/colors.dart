ebb5c611dd89f27bdb24f27d2755da79135d71c5
wilzix <nodirbarotov0707@gmail.com>
2026-03-27 22:36:30 +0500

    first commit

---
+++ lib/uikit/tokens/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static Color lightBg = Color(0xFFFCFCFA);
+  static Color primary = Color(0xFF9AE600);
+  static Color grey = Color(0xFF898D82);
+  static Color black = Color(0xFF2B2F23);
+  static Color red = Color(0xFFE96069);
+  static Color mutedBlack = Color(0xFF64685C);
+  static Color orange = Color(0xFFEEA740);
+  static Color white = Color(0xFFFFFFFF);
+  static Color borderColor = Color(0xFFE1E3DC);
+  static Color primaryDart = Color(0xFF7CCF00);
+  static Color lightGreen = Color(0xFFF5FCE8);
+  static LinearGradient blackGradient = LinearGradient(colors: [Color(0x0ff3f3f6), Color(0xFF171717)]);
+}
