a27830f3a55b19e37ece7ad7a1c81ed0a3624a6f
wilzix <nodirbarotov0707@gmail.com>
2026-03-29 20:02:51 +0500

    get it

--- lib/presentation/login/ui/sms_confirmation_page.dart
+++ lib/presentation/login/ui/sms_confirmation_page.dart
@@ -1,5 +1,6 @@
 import 'package:brandface/uikit/tokens/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
 
 import '../../../core/i18n/strings.g.dart';
 import '../../../uikit/components/buttons/buttons.dart';
@@ -29,7 +30,34 @@ class _SmsConfirmationPageState extends State<SmsConfirmationPage> {
           children: [
             Spacer(),
             Text('SMS confirmation', style: Typographies.headlineSmall),
+            SizedBox(height: 8),
+            Text(
+              'We have sent SMS code to your phone number **${widget.arguments.phoneNumber.replaceAll(' ', '').substring(widget.arguments.phoneNumber.length - 4)},please enter this code',
+              style: Typographies.bodySmall,
+            ),
             SizedBox(height: 24),
+            Pinput(
+              length: 6,
+              defaultPinTheme: PinTheme(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: AppColors.borderColor, width: 1),
+                ),
+                width: 64,
+                height: 64,
+              ),
+              focusedPinTheme: PinTheme(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: AppColors.primaryDark, width: 1),
+                ),
+                width: 64,
+                height: 64,
+              ),
+            ),
+            SizedBox(height: 24),
+            Text('00:59', style: Typographies.bodyMedium),
+            Spacer(),
             AppButtons.primary(title: t.onboarding.kContinue, onTap: () {}),
             SizedBox(height: 16 + MediaQuery.of(context).padding.bottom),
           ],
